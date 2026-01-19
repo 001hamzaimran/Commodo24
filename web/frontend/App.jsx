ffc48af53c677c61c003daf3bb8270dc58c3af0c
001hamzaimran <syed.hamza2004@gmail.com>
2026-01-19 15:52:52 +0500

    Push

---
+++ web/frontend/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { NavMenu } from "@shopify/app-bridge-react";
+import Routes from "./Routes";
+
+import { QueryProvider, PolarisProvider } from "./components";
+import CSV from "./pages/CSV";
+
+export default function App() { 
+  // Any .tsx or .jsx files in /pages will become a route
+  // See documentation for <Routes /> for more info
+  const pages = import.meta.glob("./pages/**/!(*.test.[jt]sx)*.([jt]sx)", {
+    eager: true,
+  });
+  const { t } = useTranslation();
+
+  return (
+    <PolarisProvider>
+      <BrowserRouter>
+        <QueryProvider>
+          <NavMenu>
+            <Link to="/" rel="home" />
+            <Link to="/CSV" element={<CSV />} > ORDER </Link>
+            {/* <Link to="/pagename">{t("NavigationMenu.pageName")}</Link> */}
+          </NavMenu>
+          <Routes pages={pages} />
+        </QueryProvider>
+      </BrowserRouter>
+    </PolarisProvider>
+  );
+} 
